3ec315b8be1159a28d53b470f1e22ebc71b89a15
raulordas <ordasraul@gmail.com>
2019-05-17 13:26:58 +0200

    more

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyANACH_YxDPlecd0MKhJMT5vH1UjlfRwf8",
+    authDomain: "moveyourschool-c3483.firebaseapp.com",
+    databaseURL: "https://moveyourschool-c3483.firebaseio.com",
+    projectId: "moveyourschool-c3483",
+    storageBucket: "moveyourschool-c3483.appspot.com",
+    messagingSenderId: "342823179170"
+  }
 };
